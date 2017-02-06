360425a38cae40bd256ab2925d1e34474c3799c2
Adam Yee <adamgyee@umich.edu>
2017-02-06 13:45:40 -0500

    bar graph working, working on click events

--- hw2/script.js
+++ hw2/script.js
@@ -12,16 +12,140 @@ function updateBarChart(selectedDimension) {
         xAxisWidth = 100,
         yAxisHeight = 70;
 
-    // ******* TODO: PART I *******
+    var svg = d3.select("#barChart")
+      .attr("style", "backgroundColor: blue");
+      console.log(svg);
+    var width = 500;
+    var height = 400;
+    var padding = 3;
 
+    console.log(svgBounds);
+
+    var data = [];
+    var years = [];
+    for (var i = 0; i < allWorldCupData.length; i++){
+      data.unshift(allWorldCupData[i][selectedDimension]);
+      years.unshift(allWorldCupData[i]["year"]);
+    }
+    console.log(data);
+    console.log(years);
+
+    // ******* TODO: PART I *******
+    var spacing = svgBounds["width"] / data.length;
     // Create the x and y scales; make
     // sure to leave room for the axes
 
+    // wanna use a categorical scale instead of linear!!
+    // banded scale: d3.scaleBand().domain(years).range([0,svgBounds["width"]])
+    // .padding(.2) <=- padding between bars
+    var xScale = d3.scaleBand()
+      .domain(years)
+      .range([0, svgBounds["width"] - xAxisWidth])
+      .padding(.05);
+            /*
+    var xScale = d3.scaleLinear()
+      .domain([d3.min(years), d3.max(years)])
+      .range([0, svgBounds["width"] - padding])
+      .nice();
+      */
+    /* mine pre lecture
+    var xScale = d3.scaleLinear()
+      .domain([0, allWorldCupData.length])
+      .range([5, svgBounds["width"] - 90])
+      .nice();
+
+    //var minYear = d3.min(allWorldCupData, function(d){ return d.year; }); This loops through the world cup data and retuns minimum value
+    var xScaleAxis = d3.scaleLinear()
+      .domain([d3.min(years), d3.max(years)])
+      .range([0, svgBounds["width"]])
+      .nice();
+      */
+
+    var yScale = d3.scaleLinear()
+      .domain([0,d3.max(data)])
+      .range([svgBounds["height"] - yAxisHeight,0])
+      .nice();
+
+    /* mine pre-lecture
+    var yScale = d3.scaleLinear()
+      .domain([0,d3.max(data)])
+      .range([0, svgBounds["height"]-70])
+      .nice();
+
+
+    var yScaleAxis = d3.scaleLinear()
+      .domain([d3.max(data), 0])
+      .range([0, svgBounds["height"]-70])
+      .nice();
+      */
+
+    var xAxis = d3.axisBottom(xScale)
+      .ticks(allWorldCupData.length)
+      .tickValues(years)
+      .tickFormat(d3.format("0000")).ticks(years.length);
+
+
+    var yAxis = d3.axisLeft(yScale);
+      /*.ticks(10)
+      .tickValues(data);*/
+
     // Create colorScale
+    var colorScale = d3.scaleLinear()
+      .domain([d3.min(data), d3.max(data)])
+      .range(["lightblue", "blue", "darkblue"]);
 
     // Create the axes (hint: use #xAxis and #yAxis)
+    var horizontal = d3.select("#xAxis")
+      .attr("transform", "translate(70, 350)")
+      .call(xAxis)
+      .selectAll("text")
+      .attr("y", 0)
+      .attr("x", 9)
+      .attr("dy", ".35em")
+      .attr("transform", "rotate(90)")
+      .style("text-anchor", "start");;
+
+
+    var vertical = d3.select("#yAxis")
+      .attr("transform", "translate(70,20)")
+      .transition().duration(2500)
+      .call(yAxis);
 
     // Create the bars (hint: use #bars)
+    var g = d3.select("#bars");
+    var bars = g.selectAll("rect")
+      .data(allWorldCupData);
+
+    bars.enter().append("rect").classed("bars",true)
+      .attr("x", function(d,i){
+        return xScale(years[i]) + padding;
+      })
+      .attr("y", function(d, i){
+        return yScale(data[i])
+      })
+      .attr("width", 17)
+      .merge(bars)
+      .attr("transform", "translate(70, 20)")
+      .transition().duration(2500)
+      .attr("x", function (d, i) {
+          return xScale(years[i]) + padding;
+      })
+      .attr("y", function (d, i) {
+          return yScale(data[i]);
+          //svgBounds["width"] / allWorldCupData.length;
+      })
+      .attr("width", 17)
+      .attr("height", function(d,i){
+          console.log(d);
+          return svgBounds["height"] - yScale(data[i]) - yAxisHeight;
+      })
+      .attr("fill", function (d,i) {
+          return colorScale(data[i]);
+      });
+
+
+
+
 
     // ******* TODO: PART II *******
 
@@ -44,6 +168,9 @@ function chooseData() {
     // ******* TODO: PART I *******
     //Changed the selected data when a user selects a different
     // menu item from the drop down.
+    var selected = document.getElementById('dataset');
+    updateBarChart(selected.options[selected.selectedIndex].value);
+//console.log(sel.options[sel.selectedIndex].value)
 
 }
 
